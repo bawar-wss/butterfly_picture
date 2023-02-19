3060a61e925d4c5426554bf3dc655eece86ad87d
Bawar <bawar.wss@gmail.com>
2023-02-19 12:08:13 -0800

    End: butterfly picture project

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      backgroundColor: const Color.fromRGBO(249, 244, 233, 1.0),
-      appBar: AppBar(
-        title: const Text('Butterfly'),
-        backgroundColor: Colors.deepPurpleAccent[200],
-      ),
-      body: Center(
-        child: Image.asset('assets/images/butterfly.gif'),
-      ),
-    )),
+          //backgroundColor: const Color.fromRGBO(249, 244, 233, 1.0),
+          backgroundColor: Colors.deepPurpleAccent[200],
+          appBar: AppBar(
+            actions: [Image.asset('assets/images/butterfly.ico')],
+            //leading: Image.asset('assets/images/butterfly.ico'),
+            title: const Text('Butterfly'),
+            backgroundColor: Colors.deepPurpleAccent[700],
+          ),
+          body: Center(
+            child: Image.asset(
+              'assets/images/butterfly.gif',
+            ),
+          ),
+        )),
   );
 }
